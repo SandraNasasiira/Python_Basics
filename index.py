c8c7cbbfd27b89e1da4f6133c4248abd3afe3571
Sandra Nasasiira <sandranasasiiraresty@gmail.com>
2021-03-31 21:38:23 +0300

    show

---
+++ index.py
@@ -0,0 +1,23 @@
+x=[30,56,470,45,19,51,]
+y=["python","html","javascript","kotlin"]
+#string length
+print(len(y))
+print('6'in x)
+print(x[3])
+print(y[-3])
+print(x!=y)
+print(y==x)
+b=x+y
+print(b)
+modulus=[a%7 for a in x]
+print(modulus)
+y.append("java")
+print(y)
+# for loop operation
+for words in y:print(words.upper())
+#string formating
+Greeting="How are you"
+About_me="I am  Sandra"
+Work="Backend developer"
+print(f'hello {Greeting} {About_me}  and I am a {Work}')
+
